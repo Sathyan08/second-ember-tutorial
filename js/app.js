9af4ca43ac0219d9d625c81e50c55849d49a773d
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-03-02 17:13:18 -0500

    Added fixture data

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 window.Todos = Ember.Application.create();
  // This line creates the initial instance of the Ember Object,
  // and it creates an (instance?) variable for the todos
+ Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+ // This code creates the application adapter, which either connects the app to a backend
+ // or (in this case) fixture data.
--- js/models/todo.js
+++ js/models/todo.js
@@ -5,3 +5,21 @@ Todos.Todo = DS.Model.extend({
 
 // This code creates a Todo model within the application namespace
 // and gives the model two attributes
+Todos.Todo.FIXTURES = [
+ {
+   id: 1,
+   title: 'Learn Ember.js',
+   isCompleted: true
+ },
+ {
+   id: 2,
+   title: '...',
+   isCompleted: false
+ },
+ {
+   id: 3,
+   title: 'Profit!',
+   isCompleted: false
+ }
+];
+// This code is fixture data.

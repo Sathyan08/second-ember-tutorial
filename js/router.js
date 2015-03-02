496b906c026e6de5eaaaba2e9c81e8abd6e5e703
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2015-03-02 17:50:38 -0500

    changed the router.js file to retrieve the todo model from storage and the index.html file to iterate over the stored todo models rather than static html, hard-coded todos

--- js/router.js
+++ js/router.js
@@ -5,3 +5,9 @@ Todos.Router.map(function() {
 // This code looks for the todos variable in the Ember Application Object and then
 // creates an index route for the todo variable.  In this case, the router specifies
 // that it will render the todos template on the root page rather than /todos.
+Todos.TodosRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('todo');
+  }
+});
+// This code pulls the todo models from storage
